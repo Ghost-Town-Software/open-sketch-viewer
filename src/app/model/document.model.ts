2d79ab4ecf0190d490f26555d710e7e62bfe0a42
Piotr Sałkowski <ps@mosqi.to>
2019-10-30 15:38:40 +0100

    Update some models from document.json (sketch file)

---
+++ src/app/model/document.model.ts
@@ -0,0 +1,48 @@
+export class Document {
+  readonly _class: string = 'document';
+  assets: any[];
+  colorSpace: number;
+  currentPageIndex: number;
+  do_objectID: string;
+  foreignLayerStyles: any[];
+  foreignSymbols: any[];
+  foreignTextStyles: any[];
+  layerStyles: SharedStyleContainer;
+  layerSymbols: SymbolContainer;
+  layerTextStyles: SharedTextStyleContainer;
+  pages: Page[];
+  userInfo: any; // not used
+}
+
+export class Page {
+  readonly _class: 'MSJSONFileReference';
+  _ref: string;
+  _ref_class: string;
+}
+
+export class SharedTextStyleContainer {
+  readonly _class: string = 'sharedTextStyleContainer';
+  objects: any[];
+}
+
+export class SharedStyleContainer {
+  readonly _class: string = 'sharedStyleContainer';
+  objects: any[];
+}
+
+export class SharedStyle {
+  readonly _class: string = 'sharedStyle';
+
+  do_objectID: string;
+  name: string;
+  value: {};
+}
+
+export class Style {
+
+}
+
+export class SymbolContainer {
+  readonly _class: string = 'symbolContainer';
+  objects: any[];
+}
