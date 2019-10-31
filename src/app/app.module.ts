b9baff4cfcdacaa9d53f52798771ef224b3bd0cf
sdrozdz <seba987xdc@gmail.com>
2019-10-31 10:31:52 +0100

    Added components for workspace page structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ProjectComponent} from "./components/project/project.component";
+import {WorkspaceComponent} from './components/workspace/workspace.component';
+import {AttributesPanelComponent} from './components/attributes-panel/attributes-panel.component';
+import {StructurePanelComponent} from './components/structure-panel/structure-panel.component';
+import {PageComponent} from './components/page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectComponent
+    ProjectComponent,
+    WorkspaceComponent,
+    AttributesPanelComponent,
+    StructurePanelComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/page/page.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'page',
+  templateUrl: './page.template.html',
+  styleUrls: ['./page.styles.scss']
+})
+export class PageComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
